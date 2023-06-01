eaf736b714e0f8d82f95b53616f31c49df471c65
laurent.lin <laurent.lin@aswatson.com>
2023-06-01 13:59:01 +0800

    提交代码

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <div class="header-item" v-for="(item, index) in headerList" :key="index">{{ item.label }}</div>
+  </div>
+  <div class="main"></div>
+  <div class="footer"></div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const headerList = ref([
+  {
+    label: 'diary'
+  },
+  {
+    label: 'tools'
+  }
+])
+</script>
+
+<style lang="less">
+.header {
+  display: flex;
+  border-bottom: 1px solid #ccc;
+  .header-item {
+    width: 100px;
+    height: 40px;
+    text-align: center;
+    line-height: 40px;
+    margin-right: 10px;
+    cursor: pointer;
+    &:hover {
+      background: #ffc;
+    }
+  }
+}
+</style>

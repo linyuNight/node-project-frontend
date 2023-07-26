e88436bbeab977016d9c93ce5df7f94ae306cba7
laurent.lin <laurent.lin@aswatson.com>
2023-07-26 16:29:56 +0800

    修改音乐逻辑

--- src/views/home/music/index.vue
+++ src/views/home/music/index.vue
@@ -1,15 +1,46 @@
 <template>
-  <div class="music-list">
-    <div class="item" v-for="(item, index) in musicList" :key="index">
-      <div>{{ item.name }}</div>
-      <el-button type="primary" @click="play(item)">播放</el-button>
+  <div class="music">
+    <div>音乐上传</div>
+    <el-upload
+      ref="uploadRef"
+      class="upload-demo"
+      :action="`${baseUrl}/upload_music`"
+      :auto-upload="false"
+      :headers="{
+        Authorization: authorization
+      }"
+      multiple
+      :data="uploadData"
+      :on-success="handlerSuccess"
+    >
+      <template #trigger>
+        <el-button type="primary">select file</el-button>
+      </template>
+
+      <el-button class="ml-3" type="success" @click="submitUpload">
+        upload to server
+      </el-button>
+
+      <template #tip>
+        <div class="el-upload__tip">
+          jpg/png files with a size less than 500kb
+        </div>
+      </template>
+    </el-upload>
+    <div class="music-list">
+      <div class="item" v-for="(item, index) in musicList" :key="index">
+        <div>{{ item.name }}</div>
+        <el-button type="primary" @click="play(item)">播放</el-button>
+      </div>
     </div>
-  </div>
-  <audio ref="audioRef" :src="audioSrc" controls></audio>
+    <audio ref="audioRef" :src="audioSrc" controls></audio>
+  </div>  
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref, nextTick } from 'vue'
+import { baseUrl } from '@/config/index'
+import type { UploadInstance } from 'element-plus'
 import {
   queryMusic,
   getMusic
@@ -17,44 +48,69 @@ import {
 import { GlobalStore } from "@/stores";
 const globalStore = GlobalStore();
 
+const authorization = localStorage.getItem('token')
+
+const uploadData = ref({} as any)
+
 const musicList = ref([] as any)
 
 const audioRef = ref(null as any);
 
+const uploadRef = ref<UploadInstance>()
+
 onMounted(() => {
+  getData()
+})
+
+const getData = () => {
   queryMusic({
     userid: globalStore.user.userid
-  }).then(res => {
+  }).then((res: any) => {
     console.log('test res', res)
     musicList.value = res.files
   })
-})
+}
+
+// 上传成功回调
+const handlerSuccess = () => {
+  getData()
+}
+
+// 上传
+const submitUpload = () => {
+  uploadRef.value!.submit()
+}
 
 const audioSrc = ref('' as any); // 后端提供的音频接口
 
 // 播放音乐
 const play = async (item: any) => {
-  try {
-    const response: any = await getMusic({
-      userid: globalStore.user.userid,
-      filename: item.name,
-      path: 'music'
-    });
+  audioSrc.value = getMusic(item.name)
 
-    if (!response.ok) {
-      throw new Error('请求失败');
-    }
+  nextTick(() => {
+    audioRef.value.play()
+  })
+  // try {
+  //   const response: any = await getMusic({
+  //     userid: globalStore.user.userid,
+  //     filename: item.name,
+  //     path: 'music'
+  //   });
 
-    const blob = await response.blob();
-    const url = URL.createObjectURL(blob);
+  //   if (!response.ok) {
+  //     throw new Error('请求失败');
+  //   }
 
-    audioSrc.value = url
-    nextTick(() => {
-      audioRef.value.play()
-    })
-  } catch (error) {
-    console.error('播放音频失败：', error);
-  }
+  //   const blob = await response.blob();
+  //   const url = URL.createObjectURL(blob);
+
+  //   audioSrc.value = url
+  //   nextTick(() => {
+  //     audioRef.value.play()
+  //   })
+  // } catch (error) {
+  //   console.error('播放音频失败：', error);
+  // }
 }
 </script>
 
